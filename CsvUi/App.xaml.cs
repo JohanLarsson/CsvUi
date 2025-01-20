050d2e2ea27faee16f0c3577d4477a91272cfed1
Johan Larsson <johan_gavle1@hotmail.com>
2025-01-20 19:49:11 +0100

    initial

---
+++ CsvUi/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace CsvUi;
+
+using System.Windows;
+
+public partial class App : Application
+{
+}
---
+++ CsvUi/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CsvUi;
+
+using Microsoft.Win32;
+using System.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private void OnOpenClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Filter = "csv files|*.csv",
+        };
+
+        if (dialog.ShowDialog(this) is true)
+        {
+            this.DataGrid.ItemsSource = new CsvView(Csv.Read(System.IO.File.ReadAllText(dialog.FileName), ','));
+        }
+    }
+}
